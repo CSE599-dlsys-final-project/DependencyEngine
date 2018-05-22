a1911ff15abfaa676f5a78f1f336bda776c0bf50
David Grant <dagr@cs.washington.edu>
2018-05-21 18:46:07 -0700

    distutils setup script just so that I can run run the test scripts easier...

---
+++ python/setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='DlSysDependencyEngine',
+      version='0.9',
+      description='Dependency engine for computation graph executor'
+     )
