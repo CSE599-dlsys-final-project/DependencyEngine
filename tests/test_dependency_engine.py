5ed4ed5cae73ae3d3ef0061217b20c0fd50b18ad
David Grant <dagr@cs.washington.edu>
2018-05-22 18:32:39 -0700

    Use existing test helpers from mnist_dlsys.py.

--- tests/test_dependency_engine.py
+++ tests/test_dependency_engine.py
@@ -11,6 +11,7 @@ import tvm
 
 from dlsys import autodiff, tvm_op, dependency_engine
 ad = autodiff
+from mnist_dlsys import load_mnist_data, convert_to_one_hot
 
 
 tgt_host="llvm"
@@ -58,54 +59,6 @@ def test_matrix_elementwise_add_naive():
     for _ in range(5):
         engine.naive_executor()
 
-def load_mnist_data(dataset):
-    """ Load the dataset
-    Code adapted from http://deeplearning.net/tutorial/code/logistic_sgd.py
-
-    :type dataset: string
-    :param dataset: the path to the dataset (here MNIST)
-    """
-    # Download the MNIST dataset if it is not present
-    data_dir, data_file = os.path.split(dataset)
-    if data_dir == "" and not os.path.isfile(dataset):
-        # Check if dataset is in the data directory.
-        new_path = os.path.join(
-            os.path.split(__file__)[0],
-            dataset
-        )
-        if os.path.isfile(new_path) or data_file == 'mnist.pkl.gz':
-            dataset = new_path
-
-    if (not os.path.isfile(dataset)) and data_file == 'mnist.pkl.gz':
-        from six.moves import urllib
-        origin = (
-            'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
-        )
-        print('Downloading data from %s' % origin)
-        urllib.request.urlretrieve(origin, dataset)
-
-    print('Loading data...')
-
-    # Load the dataset
-    with gzip.open(dataset, 'rb') as f:
-        try:
-            train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
-        except:
-            train_set, valid_set, test_set = pickle.load(f)
-    # train_set, valid_set, test_set format: tuple(input, target)
-    # input is a numpy.ndarray of 2 dimensions (a matrix), np.float32
-    # where each row corresponds to an example. target is a
-    # numpy.ndarray of 1 dimension (vector), np.int64 that has the same length
-    # as the number of rows in the input. It should give the target
-    # to the example with the same index in the input.
-    return train_set, valid_set, test_set
-
-def convert_to_one_hot(vals):
-    """Helper method to convert label array to one-hot array."""
-    one_hot_vals = np.zeros((vals.size, vals.max()+1))
-    one_hot_vals[np.arange(vals.size), vals] = 1
-    return one_hot_vals
-
 def test_mnist_logreg():
     tgt = "llvm"
     tgt_host = "llvm"
@@ -114,7 +67,6 @@ def test_mnist_logreg():
     executor_ctx = tvm.context(tgt, 0)
     print_loss_val_each_epoch = True
     num_epochs = 10
-
 
     print("=== Build logistic regression model...")
 
