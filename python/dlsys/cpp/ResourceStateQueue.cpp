776cb67d637bd502f9bbb9009312821e7e8a948f
BBBBlarry <blarry.personal@gmail.com>
2018-05-27 18:05:23 -0700

    added state and state transition logic

--- python/dlsys/cpp/ResourceStateQueue.cpp
+++ python/dlsys/cpp/ResourceStateQueue.cpp
@@ -43,3 +43,51 @@ void ResourceStateQueue::stopListening() {
 bool ResourceStateQueue::handleNextPendingInstruction() {
     return false;
 }
+
+void ResourceStateQueue::toState(ResourceStateQueue::State state) {
+    std::lock_guard<std::mutex> stateLock(this->stateMutex);
+    this->priv_toState(state);
+}
+
+void ResourceStateQueue::priv_toState(ResourceStateQueue::State state) {
+    // retrieve it only once to save time
+    auto s = this->state;
+    if (s == ResourceStateQueue::N) {
+        if (state == ResourceStateQueue::MR) {
+            std::cerr << "Invalid state transition" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    } else if (s == ResourceStateQueue::R) {
+        if (state == ResourceStateQueue::N) {
+            std::cerr << "Invalid state transition" << std::endl;
+            exit(EXIT_FAILURE);
+        } else if (state == ResourceStateQueue::R) {
+            this->pastRChainLength++;
+        } else { // MR
+            if (this->pastRChainLength != 0) {
+                std::cerr << "Invalid state transition" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+    } else { // MR
+        if (state == ResourceStateQueue::R) this->pastRChainLength++;
+    }
+    // update the state
+    this->state = state;
+}
+
+void ResourceStateQueue::restoreState() {
+  std::lock_guard<std::mutex> stateLock(this->stateMutex);
+  auto s = this->state;
+  if (s == ResourceStateQueue::MR) {
+      std::cerr << "Invalid state restoration" << std::endl;
+      exit(EXIT_FAILURE);
+  } else if (s == ResourceStateQueue::R) {
+      this->pastRChainLength--;
+      if (this->pastRChainLength == 0) {
+          this->priv_toState(ResourceStateQueue::MR);
+      }
+  } else { // N
+      this->priv_toState(ResourceStateQueue::MR);
+  }
+}
--- python/dlsys/cpp/ResourceStateQueue.hpp
+++ python/dlsys/cpp/ResourceStateQueue.hpp
@@ -4,14 +4,18 @@
 #include <mutex>
 #include <condition_variable>
 #include <memory>
+#include <iostream>
 
 #include "Instruction.hpp"
 
 class ResourceStateQueue {
 public:
     ResourceStateQueue(const std::atomic<bool>& shouldStop, long tag)
-        : shouldStop(shouldStop), tag(tag)
-    { }
+        : shouldStop(shouldStop), tag(tag), state(MR)
+    {  }
+
+    // represent the state of a resource-state queue
+    enum State {N, R, MR};
 
     void push(std::shared_ptr<Instruction> instruction);
     void listen();
@@ -20,10 +24,20 @@ public:
     void stopListening();
     bool handleNextPendingInstruction();
 
+    // state and transitions
+    void toState(ResourceStateQueue::State state);
+    void restoreState();
+
 private:
     std::queue<std::shared_ptr<Instruction>> queue;
     std::mutex queueMutex;
     std::condition_variable queueActivity;
     const std::atomic<bool>& shouldStop;
     const long tag;
+    std::mutex stateMutex;
+    // the current state of the resource-state queue
+    ResourceStateQueue::State state;
+    void priv_toState(ResourceStateQueue::State state);
+    // number of R states
+    short pastRChainLength;
 };
