4e42ee7dc598602c77aeefb1bf2a68ec3ce35727
David Grant <dagr@cs.washington.edu>
2018-05-28 20:21:05 -0700

    Fix thing where setup.py wasn't picking up on changed headers.

--- python/setup.py
+++ python/setup.py
@@ -8,11 +8,13 @@ from setuptools.extension import Extension
 from Cython.Build import cythonize
 
 cpp_source_files = glob.glob("dlsys/cpp/*.cpp")
+cpp_depends = glob.glob("dlsys/cpp/*.hpp")
 
 cppExtension = Extension(
     "dependencyengine",
     ["dlsys/*.pyx"] + cpp_source_files,
     include_dirs=[".", "dlsys/cpp"],
+    depends=cpp_depends,
     extra_compile_args=['-g', '-std=c++14', '-stdlib=libc++']
 )
 
